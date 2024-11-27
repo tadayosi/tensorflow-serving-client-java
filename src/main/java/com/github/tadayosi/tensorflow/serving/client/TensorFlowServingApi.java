9692323528f67e4f97cc07df48ee8b6ad8f32784
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-11-27 14:54:17 +0900

    add javadoc to the API class

--- src/main/java/com/github/tadayosi/tensorflow/serving/client/TensorFlowServingApi.java
+++ src/main/java/com/github/tadayosi/tensorflow/serving/client/TensorFlowServingApi.java
@@ -8,13 +8,31 @@ import tensorflow.serving.RegressionOuterClass;
 
 public interface TensorFlowServingApi {
 
+    /**
+     * Gets status of model. If the ModelSpec in the request does not specify
+     * version, information about all versions of the model will be returned. If
+     * the ModelSpec in the request does specify a version, the status of only
+     * that version will be returned.
+     */
     GetModelStatus.GetModelStatusResponse getModelStatus(GetModelStatus.GetModelStatusRequest request);
 
+    /**
+     * GetModelMetadata - provides access to metadata for loaded models.
+     */
     GetModelMetadata.GetModelMetadataResponse getModelMetadata(GetModelMetadata.GetModelMetadataRequest request);
 
+    /**
+     * Classify.
+     */
     Classification.ClassificationResponse classify(Classification.ClassificationRequest request);
 
+    /**
+     * Regress.
+     */
     RegressionOuterClass.RegressionResponse regress(RegressionOuterClass.RegressionRequest request);
 
+    /**
+     * Predict -- provides access to loaded TensorFlow model.
+     */
     Predict.PredictResponse predict(Predict.PredictRequest request);
 }
